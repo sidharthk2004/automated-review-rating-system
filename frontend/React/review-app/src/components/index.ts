fe8bb0fe1c48e86d69cecd0ab1cb9b848566c3b6
sidharthk2004 <sidharth02004@gmail.com>
2025-10-10 23:32:40 +0530

    Add files via upload

---
+++ frontend/React/review-app/src/components/index.ts
@@ -0,0 +1,25 @@
+export { default as Header } from "./ui/Header";
+export { default as Footer } from "./ui/Footer";
+
+export { default as Loading } from "./ui/Laoading";
+
+export { Button } from "./ui/button";
+export { Label } from "./ui/label";
+export { Badge } from "./ui/badge";
+export { Input } from "./ui/input";
+export { Card, CardContent, CardHeader, CardTitle } from "./ui/card";
+export { Progress } from "./ui/progress";
+export { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+export { Textarea } from "./ui/textarea";
+export {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectScrollDownButton,
+  SelectScrollUpButton,
+  SelectSeparator,
+  SelectTrigger,
+  SelectValue,
+} from "./ui/select";

6b0d7e2cfc463dd788f8ead42896c83949d28f12
sidharthk2004 <sidharth02004@gmail.com>
2025-10-10 23:29:30 +0530

    Add files via upload

---
+++ frontend/React/review-app/src/App.tsx
@@ -0,0 +1,12 @@
+import React, { type JSX } from "react";
+import AppRoutes from "./routes/AppRoutes";
+
+const App: React.FC = (): JSX.Element => {
+  return (
+    <>
+      <AppRoutes />
+    </>
+  );
+};
+
+export default App;

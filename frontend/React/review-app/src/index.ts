d11148833dfa21ce6c53ca24efe22785cad3e465
sidharthk2004 <sidharth02004@gmail.com>
2025-10-10 23:35:36 +0530

    Add files via upload

---
+++ frontend/React/review-app/src/index.ts
@@ -0,0 +1,5 @@
+export {default as ShowReviewPage} from './pages/ShowReviewPage'
+export {default as ReviewCard} from './components/ReviewCard'
+export {default as RenderStars}from "./components/RenderStars"
+export {default as ReviewForm } from "./components/ReviewForm"
+
